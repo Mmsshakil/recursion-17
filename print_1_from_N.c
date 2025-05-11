882b9ece812ce3d9edc2bf25b1275c6e84324261
Mmsshakil <softhandshakil@gmail.com>
2025-05-11 17:02:29 +0600

    print n to 1 by recursion

--- print_1_from_N.c
+++ print_1_from_N.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
-void numbers(int i)
+void numbers(int i, int n)
 {
-    if (i == 5)
+    if (i == n + 1)
     {
         return;
     }
 
     printf("%d\n", i);
-    numbers(i + 1);
+    numbers(i + 1, n);
 }
 
 int main()
 {
-    int i = 1;
-    numbers(i);
+    int i = 1, n;
+    scanf("%d", &n);
+    numbers(i, n);
     return 0;
 }
